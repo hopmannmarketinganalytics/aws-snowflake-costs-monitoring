034a12168dd50bc25ffd44b26aab8a5ead56f64d
federicoerroi <federico.erroi@hopmann.net>
2025-10-06 17:03:36 +0200

    Updated code to make it environment-agnostic

--- main.tf
+++ main.tf
@@ -4,13 +4,8 @@ terraform {
       source  = "hashicorp/aws"
       version = ">= 4.20.0"
     }
-
   }
-  backend "s3" {
-    bucket                  = "terraform-s3-state-4637483747" # needs to be hardcoded and created manually
-    key                     = "terraform.tfstate"
-    region                  = "eu-central-1"
-  }
+  backend "s3" {}
 }
 
 provider "aws" {
